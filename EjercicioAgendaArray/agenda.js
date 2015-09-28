022358529cd488d803a33a431ad40c26ad17547c
luis <luis.gil@tajamar365.com>
2015-09-28 14:22:50 +0200

    Terminado

---
+++ EjercicioAgendaArray/agenda.js
@@ -0,0 +1,28 @@
+﻿var data = [];
+function escribir() {
+
+    var capa = document.getElementById("capaAgenda");
+    capa.innerHTML = "";
+
+    for (var i = 0; i < data.length; i++) {
+        capa.innerHTML +=i+"-->"+ data[i] + "<br />";
+    }
+}
+
+function add() {
+    var texto = document.getElementById("txtNombre");
+    data.push(texto.value);
+    texto.value = "";
+    escribir();
+}
+
+function borrar() {
+    var aborrar = document.getElementById("txtBorrar");
+
+    data.splice(aborrar.value, 1);
+    aborrar.value = "";
+
+    escribir();
+}
+
+
